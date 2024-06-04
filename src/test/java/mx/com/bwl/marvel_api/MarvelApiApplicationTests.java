54a3e48ac762cd49d87a9cc224fb15855b75785d
Mario Ledesma <miledesmam@gmail.com>
2024-06-03 19:32:08 -0600

    Se agregan tests

--- src/test/java/mx/com/bwl/marvel_api/MarvelApiApplicationTests.java
+++ src/test/java/mx/com/bwl/marvel_api/MarvelApiApplicationTests.java
@@ -1,13 +1,40 @@
 package mx.com.bwl.marvel_api;
 
+import mx.com.bwl.marvel_api.service.MarvelApiService;
+import mx.com.bwl.marvel_api.service.impl.MarvelApiServiceImpl;
+import mx.com.bwl.marvel_api.vo.marvel.ResponseVO;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest
+
+@TestComponent
 class MarvelApiApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    void findAllCharacters(){
+        MarvelApiService marvelApiService = new MarvelApiServiceImpl("a6c573c4a1a36fff296ab0c545b30a58", "9b43537b543599a78d1974154955946d", "1716933812");
+        assertThat(marvelApiService.findAllCharacters())
+                .isNotNull()
+                .isInstanceOf(ResponseVO.class)
+                .matches(responseVO -> responseVO.getData().getCount() == 20);
+
+    }
+
+    @Test
+    void findCharacterById(){
+        MarvelApiService marvelApiService = new MarvelApiServiceImpl("a6c573c4a1a36fff296ab0c545b30a58", "9b43537b543599a78d1974154955946d", "1716933812");
+        ResponseVO resp = marvelApiService.findCharacterById(1011136L);
+        assertThat(resp)
+                .isNotNull()
+                .isInstanceOf(ResponseVO.class)
+                .matches(responseVO -> responseVO.getData().getCount() == 1);
+    }
 
 }
