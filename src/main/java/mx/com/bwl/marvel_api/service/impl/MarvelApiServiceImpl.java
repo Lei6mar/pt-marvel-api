fe61d67854ee84502a9c32ccc5692b8dc38339c0
Mario Ledesma <miledesmam@gmail.com>
2024-06-04 18:20:57 -0600

    Se quitan variables de properties, se leen desde variables de entorno

--- src/main/java/mx/com/bwl/marvel_api/service/impl/MarvelApiServiceImpl.java
+++ src/main/java/mx/com/bwl/marvel_api/service/impl/MarvelApiServiceImpl.java
@@ -2,6 +2,7 @@ package mx.com.bwl.marvel_api.service.impl;
 
 import mx.com.bwl.marvel_api.service.MarvelApiService;
 import mx.com.bwl.marvel_api.vo.marvel.ResponseVO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 public class MarvelApiServiceImpl implements MarvelApiService {
@@ -11,9 +12,17 @@ public class MarvelApiServiceImpl implements MarvelApiService {
         this.apiPublicKey = publicKey;
         this.apiPrivateKey = privateKey;
         this.apiTimestampValue = apiTimeStamp;
+        this.apiUrl = System.getenv("API_URL");
     }
 
-    private static final String API_URL = "https://gateway.marvel.com/v1/public/characters";
+    public MarvelApiServiceImpl(String publicKey, String privateKey, String apiTimeStamp, String apiUrl){
+        this.apiPublicKey = publicKey;
+        this.apiPrivateKey = privateKey;
+        this.apiTimestampValue = apiTimeStamp;
+        this.apiUrl = apiUrl;
+    }
+
+    private final String apiUrl;
 
     private final String apiPublicKey;
 
@@ -25,12 +34,12 @@ public class MarvelApiServiceImpl implements MarvelApiService {
     @Override
     public ResponseVO findAllCharacters() {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(API_URL.concat("?apikey=").concat(apiPublicKey).concat("&ts=").concat(apiTimestampValue).concat("&hash=").concat(apiPrivateKey), ResponseVO.class);
+        return restTemplate.getForObject(apiUrl.concat("?apikey=").concat(apiPublicKey).concat("&ts=").concat(apiTimestampValue).concat("&hash=").concat(apiPrivateKey), ResponseVO.class);
     }
 
     @Override
     public ResponseVO findCharacterById(Long id) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(API_URL.concat("/").concat(String.valueOf(id)).concat("?apikey=").concat(apiPublicKey).concat("&ts=").concat(apiTimestampValue).concat("&hash=").concat(apiPrivateKey), ResponseVO.class);
+        return restTemplate.getForObject(apiUrl.concat("/").concat(String.valueOf(id)).concat("?apikey=").concat(apiPublicKey).concat("&ts=").concat(apiTimestampValue).concat("&hash=").concat(apiPrivateKey), ResponseVO.class);
     }
 }
--- src/test/java/mx/com/bwl/marvel_api/MarvelApiApplicationTests.java
+++ src/test/java/mx/com/bwl/marvel_api/MarvelApiApplicationTests.java
@@ -13,13 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestComponent
 class MarvelApiApplicationTests {
 
-    @Autowired
-    private MockMvc mvc;
-
 
     @Test
     void findAllCharacters(){
-        MarvelApiService marvelApiService = new MarvelApiServiceImpl("a6c573c4a1a36fff296ab0c545b30a58", "9b43537b543599a78d1974154955946d", "1716933812");
+        MarvelApiService marvelApiService = new MarvelApiServiceImpl("a6c573c4a1a36fff296ab0c545b30a58", "9b43537b543599a78d1974154955946d", "1716933812", "https://gateway.marvel.com/v1/public/characters");
         assertThat(marvelApiService.findAllCharacters())
                 .isNotNull()
                 .isInstanceOf(ResponseVO.class)
@@ -29,7 +26,7 @@ class MarvelApiApplicationTests {
 
     @Test
     void findCharacterById(){
-        MarvelApiService marvelApiService = new MarvelApiServiceImpl("a6c573c4a1a36fff296ab0c545b30a58", "9b43537b543599a78d1974154955946d", "1716933812");
+        MarvelApiService marvelApiService = new MarvelApiServiceImpl("a6c573c4a1a36fff296ab0c545b30a58", "9b43537b543599a78d1974154955946d", "1716933812", "https://gateway.marvel.com/v1/public/characters");
         ResponseVO resp = marvelApiService.findCharacterById(1011136L);
         assertThat(resp)
                 .isNotNull()
